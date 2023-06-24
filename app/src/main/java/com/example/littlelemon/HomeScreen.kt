658fa5b20ae3d22d8515cbbd74a9ddd07afd3f30
Belajar <bljkmp@gmail.com>
2023-06-24 08:59:12 +0700

    Create Upper Panel to show restaurant highlight

---
+++ app/src/main/java/com/example/littlelemon/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.example.littlelemon
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Column {
+        UpperPanel()
+    }
+}
